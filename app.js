f64db16bef4b95ee0d9c0ae5b3d3deb0c9ac91eb
shihabmoni15 <shihabmoni15@gmail.com>
2023-11-13 00:34:14 +0600

    first commietr

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./views/myimg");
+    cb(null, "./public/myimg");
   },
   filename: function (req, file, cb) {
     const filenamer = Date.now() + "-" + file.originalname;
@@ -46,7 +46,7 @@ app.post("/", upload.single("img"), async (req, res) => {
       attachments: [
         {
           filename: req.file.filename,
-          path: `./views/myimg/${req.file.filename}`,
+          path: `./public/myimg/${req.file.filename}`,
         },
       ], // plain text body
     });
